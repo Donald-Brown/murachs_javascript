54cc8762a357785f61ecde819564a3284e849f10
donald-brown <donbrown4652@gmail.com>
2020-08-09 16:45:08 -0500

    completes chapter 4

--- future_value/app.js
+++ future_value/app.js
@@ -1,48 +1,94 @@
-(function() {
-  let moreCalculations = "y";
-  let futureValue;
-  let interestRate;
+let $ = function(id){
+  return document.getElementById(id);
+}
 
-  do {
-    let investmentAmount = 10000;
-    let years;
+let calculateFutureValue = function(investment, interestRate, years){
+  let futureValue = investment;
+  for(let i = 1; i <= years; i++){
+    futureValue += futureValue * interestRate/100;
+  }
 
-    do {
-      investmentAmount = parseFloat(
-        prompt("Enter a investment amount as xxxxx.xx", investmentAmount)
-      );
-      if (isNaN(investmentAmount)) {
-        alert("Please enter a number");
-      }
-    } while (isNaN(investmentAmount));
+  return futureValue.toFixed(2);
+}
 
-    do {
-      interestRate = parseFloat(prompt("Enter an interest rate as xx.x", 10));
-      if (interestRate <= 0 || interestRate > 15 || isNaN(interestRate)) {
-        alert("enter a value greater than 0 and less than 15");
-      }
-    } while (interestRate <= 0 || interestRate > 15 || isNaN(interestRate));
+let processData = function(){
+  let investment = parseFloat($('total_investment').value);
+  let interestRate = parseFloat($('interest_rate').value);
+  let years = parseFloat($('years').value);
+  let isValid = true;
+  // validate investment
+  if(isNaN(investment) || investment <= 0 || investment > 1e5){
+    $('total_investment_error').firstChild.nodeValue = 'Must be > 0 and <= 100000';
+    isValid = false;
+  }
+  // validate interest rate
+  if(isNaN(interestRate) || interestRate <= 0 || interestRate > 15){
+    $('interest_rate_error').firstChild.nodeValue = 'Must be > 0 and <= 15';
+    isValid = false;
+  }
+  // validate years
+  if(isNaN(years) || years <= 0 || years > 50){
+    $('years_error').firstChild.nodeValue = `Must be > 0 and <= 50`;
+    isValid = false;
+  }
 
-    do {
-      years = parseInt(prompt("Enter number of years", 7));
-      if(years <= 0 || isNaN(years)){
-        alert("Please enter a number greater than zero")
-      }
-    } while(years <= 0 || isNaN(years));
+  if(isValid){
+    let futureValue = calculateFutureValue(investment, interestRate, years)
+    $('value').value = futureValue;
+  }
+}
 
-    futureValue = investmentAmount;
-    document.write(`Invesment Amount = ${investmentAmount} Interest Rate = ${interestRate} Years = ${years}<br>`);
-    for(let i = 1; i <= years; i++){
-      let yearlyInterest = futureValue*interestRate/100;
-      futureValue += yearlyInterest;
-      document.write(`year= ${i} interest= ${yearlyInterest} value= ${parseFloat(futureValue).toFixed(2)}<br>`); 
-    }
+window.onload = function(){
+  $('calculate').onclick = processData;
+  $('total_investment').focus();
+}
 
-    document.write('<br>')
 
-    moreCalculations = prompt(
-      "Would you like to calculate another investment? (y/n)"
-    );
-  } while (moreCalculations == "y");
-})();
+// (function() {
+//   let moreCalculations = "y";
+//   let futureValue;
+//   let interestRate;
+
+//   do {
+//     let investmentAmount = 10000;
+//     let years;
+
+//     do {
+//       investmentAmount = parseFloat(
+//         prompt("Enter a investment amount as xxxxx.xx", investmentAmount)
+//       );
+//       if (isNaN(investmentAmount)) {
+//         alert("Please enter a number");
+//       }
+//     } while (isNaN(investmentAmount));
+
+//     do {
+//       interestRate = parseFloat(prompt("Enter an interest rate as xx.x", 10));
+//       if (interestRate <= 0 || interestRate > 15 || isNaN(interestRate)) {
+//         alert("enter a value greater than 0 and less than 15");
+//       }
+//     } while (interestRate <= 0 || interestRate > 15 || isNaN(interestRate));
+
+//     do {
+//       years = parseInt(prompt("Enter number of years", 7));
+//       if(years <= 0 || isNaN(years)){
+//         alert("Please enter a number greater than zero")
+//       }
+//     } while(years <= 0 || isNaN(years));
+
+//     futureValue = investmentAmount;
+//     document.write(`Invesment Amount = ${investmentAmount} Interest Rate = ${interestRate} Years = ${years}<br>`);
+//     for(let i = 1; i <= years; i++){
+//       let yearlyInterest = futureValue*interestRate/100;
+//       futureValue += yearlyInterest;
+//       document.write(`year= ${i} interest= ${yearlyInterest} value= ${parseFloat(futureValue).toFixed(2)}<br>`); 
+//     }
+
+//     document.write('<br>')
+
+//     moreCalculations = prompt(
+//       "Would you like to calculate another investment? (y/n)"
+//     );
+//   } while (moreCalculations == "y");
+// })();
 
--- mpg/app.js
+++ mpg/app.js
@@ -7,17 +7,43 @@ let calculateMpg = function(gallons, miles){
 }
 
 let processInput = function(){
-  let miles = parseFloat($('miles').value);
-  let gallons = parseFloat($('gallons').value);
-  if(isNaN(miles) || isNaN(gallons)){
-    alert('Both inputs must be real numbers');
-  }else{
+  let miles = $('miles').value;
+  let gallons = $('gallons').value;
+  let isValid = true;
+  // validate miles
+  if(isNaN(miles)){
+    $('miles_error').firstChild.nodeValue = 'Miles must be a number';
+    isValid = false;
+  }else if(miles <= 0){
+    $('miles_error').firstChild.nodeValue = 'Miles must be greater than zero';
+    isValid = false;
+  }
+  // validate gallons
+  if(isNaN(gallons)){
+    $('gallons_error').firstChild.nodeValue = 'Gallons must be a number';
+    isValid = false;
+  }else if(gallons <= 0){
+    $('gallons_error').firstChild.nodeValue = 'Gallons must be greater than zero';
+    isValid = false;
+  }
+  if(isValid){
     let mpg = calculateMpg(gallons, miles);
     $('mpg').value = mpg;
   }
 }
 
+let clearEntries = function(){
+  $('miles').value = '';
+  $('gallons').value = '';
+  $('miles_error').firstChild.nodeValue = '*';
+  $('gallons_error').firstChild.nodeValue = '*';
+  $('mpg').value = '';
+  $('miles').focus();
+}
+
 window.onload = function(){
   $('calculate_mpg').onclick = processInput;
+  $('clear_entries').onclick = clearEntries;
+  $('miles').ondblclick = clearEntries;
   $('miles').focus(); 
 }
